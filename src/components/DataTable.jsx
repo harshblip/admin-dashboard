9df667c879be7f11c1ba179d472c43d652b08eba
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-12-02 16:05:19 +0530

    testing on search bar

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { DeleteOutlined, 
-    FormOutlined, 
-    CheckSquareOutlined, 
-    DoubleLeftOutlined, 
-    DoubleRightOutlined, 
-    LeftOutlined, 
-    RightOutlined } from '@ant-design/icons';
+import {
+    DeleteOutlined,
+    FormOutlined,
+    CheckSquareOutlined,
+    DoubleLeftOutlined,
+    DoubleRightOutlined,
+    LeftOutlined,
+    RightOutlined
+} from '@ant-design/icons';
 
 export default function DataTable() {
     const [data, setData] = useState([]);
@@ -13,6 +15,8 @@ export default function DataTable() {
     const [itemsPerPage] = useState(10);
     const [editingRows, setEditingRows] = useState({});
     const [selectedRow, setSelectedRow] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
     useEffect(() => {
         fetchEmployis();
     }, [])
@@ -71,6 +75,15 @@ export default function DataTable() {
     const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
 
+    const handleSearch = () => {
+        const filteredData = data.filter(item =>
+            Object.values(item).some(val =>
+                String(val).toLowerCase().includes(searchQuery.toLowerCase())
+            )
+        );
+        setData(filteredData);
+    };
+
     // {
     //     selectedRow.map((index) => {
     //         console.log(index);
@@ -78,86 +91,104 @@ export default function DataTable() {
     // }
 
     return (
-        <div className='ml-6 mr-6 rounded-lg mt-10 border border-[#abaaaa] text-black/70 font-bold'>
-            <table className='table'>
-                <thead>
-                    <tr className='hover:bg-slate-100 transition-all'>
-                        <input type='checkbox' alt='check' className='ml-4 mt-4'
-                            onChange={(e) => {
-                                if (e.target.checked) {
-                                    setSelectedRow(data.map((_, index) => index));
-                                } else {
-                                    setSelectedRow([]);
-                                }
-                            }}
-                        />
-                        <th scope='col' >Name</th>
-                        <th scope='col'>Email</th>
-                        <th scope='col'>Role</th>
-                        <th scope='col'>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {currentItems.map((item, index) => (
+        <>
+            <input
+                type='text'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter') {
+                        handleSearch();
+                    }
+                }}
+                placeholder='Search'
+                className='border border-[#abaaaa] text-black/70 ml-6 mt-4 w-[24rem] h-[2.5rem] rounded-md'
+            />
+            <div className='ml-6 mr-6 rounded-lg mt-4 border border-[#abaaaa] text-black/70 font-bold'>
+                <table className='table'>
+                    <thead>
                         <tr className='hover:bg-slate-100 transition-all'>
-                            <input type='checkbox' alt='check' className='ml-2 mt-4'
-                                checked={selectedRow.includes(index)}
-                                onClick={(e) => {
+                            <input type='checkbox' alt='check' className='ml-4 mt-4'
+                                onChange={(e) => {
                                     if (e.target.checked) {
-                                        setSelectedRow(prevState => [...prevState, index]);
+                                        setSelectedRow(data.map((_, index) => index));
                                     } else {
-                                        setSelectedRow(prevState => prevState.filter(i => i !== index));
+                                        setSelectedRow([]);
                                     }
                                 }}
                             />
-                            <td contentEditable={editingRows[index]}>{item.name} </td>
-                            <td contentEditable={editingRows[index]}>{item.email} </td>
-                            <td contentEditable={editingRows[index]}>{item.role} </td>
-                            <td className='flex space-x-4'>
-                                <button onClick={() => setEditingRows(prevState => ({ ...prevState, [index]: !prevState[index] }))}>
-                                    {
-                                        editingRows[index] ?
-                                            <div className='hover:bg-purple-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md'>
-                                                <CheckSquareOutlined style={{ padding: '2px' }} />
-                                            </div> :
-                                            <div className='hover:bg-purple-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md'>
-                                                <FormOutlined />
-                                            </div>
-                                    }
-                                </button>
-                                <button>
-                                    <div className='hover:bg-green-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md ' onClick={() => handleDelete(index)}>
-                                        <DeleteOutlined style={{ color: 'red' }} />
-                                    </div>
-                                </button>
-                            </td>
+                            <th scope='col' >Name</th>
+                            <th scope='col'>Email</th>
+                            <th scope='col'>Role</th>
+                            <th scope='col'>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className='mt-4 justify-around'>
-                <button className='p-2 mb-4 bg-red-500 text-white font-mono font-normal rounded-lg'
-                    onClick={() => handleDeleteSelected()}
-                >
-                    Delete Selected
-                </button>
-                <DoubleLeftOutlined  onClick={() => handleFirstPage()} className='border ml-4 mr-4'/>
-                <LeftOutlined onClick={() => handlePreviousPage()} className='border mr-4'/>
-                {pageNumbers.map((index) => {
-                    return (
-                        <button
-                            key={index}
-                            id={index}
-                            onClick={handleClick}
-                            className='border mr-4'
-                        >
-                            {index}
-                        </button>
-                    );
-                })}
-                <RightOutlined onClick={() => handleNextPage()} className='border mr-4'/>
-                <DoubleRightOutlined onClick={() => handleLastPage()} className='border mr-4'/>
+                    </thead>
+                    <tbody>
+                        {currentItems.map((item, index) => (
+                            <tr className='hover:bg-slate-100 transition-all'>
+                                <input type='checkbox' alt='check' className='ml-2 mt-4'
+                                    checked={selectedRow.includes(index)}
+                                    onClick={(e) => {
+                                        if (e.target.checked) {
+                                            setSelectedRow(prevState => [...prevState, index]);
+                                        } else {
+                                            setSelectedRow(prevState => prevState.filter(i => i !== index));
+                                        }
+                                    }}
+                                />
+                                <td contentEditable={editingRows[index]}>{item.name} </td>
+                                <td contentEditable={editingRows[index]}>{item.email} </td>
+                                <td contentEditable={editingRows[index]}>{item.role} </td>
+                                <td className='flex space-x-4'>
+                                    <button onClick={() => setEditingRows(prevState => ({ ...prevState, [index]: !prevState[index] }))}>
+                                        {
+                                            editingRows[index] ?
+                                                <div className='hover:bg-purple-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md'>
+                                                    <CheckSquareOutlined style={{ padding: '2px' }} />
+                                                </div> :
+                                                <div className='hover:bg-purple-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md'>
+                                                    <FormOutlined />
+                                                </div>
+                                        }
+                                    </button>
+                                    <button>
+                                        <div className='hover:bg-green-400 hover:text-white transition-all h-10 w-10 border border-[#abaaaa] p-1 rounded-md ' onClick={() => handleDelete(index)}>
+                                            <DeleteOutlined style={{ color: 'red' }} />
+                                        </div>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className='flex mt-4 justify-between'>
+                    <button className='p-2 mb-4 bg-red-500 text-white font-mono font-normal rounded-lg'
+                        onClick={() => handleDeleteSelected()}
+                    >
+                        Delete Selected
+                    </button>
+                    <div>
+                        {/* border ml-4 mr-4 */}
+                        <DoubleLeftOutlined onClick={() => handleFirstPage()} className='first-page' />
+                        {/* border mr-4 */}
+                        <LeftOutlined onClick={() => handlePreviousPage()} className='previous-page' />
+                        {pageNumbers.map((index) => {
+                            return (
+                                <button
+                                    key={index}
+                                    id={index}
+                                    onClick={handleClick}
+                                    className='border mr-4 p-2 h-10 w-10'
+                                >
+                                    {index}
+                                </button>
+                            );
+                        })}
+                        <RightOutlined onClick={() => handleNextPage()} className='next-page' />
+                        <DoubleRightOutlined onClick={() => handleLastPage()} className='last-page' />
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
